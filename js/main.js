225fbc5a25ef1d799800a93a38cc9c0657b5b0b1
amishagin <mishagin.2012@mail.ru>
2022-08-29 12:34:35 +0500

    add select event

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ class Item {
 }
 let data = [];
 let activeTr = null;
+let citiesSorted = [];
 
 const input = document.getElementsByClassName('Input-region')[0];
 
@@ -16,27 +17,57 @@ input.oninput = () => {
     let cities = document.getElementsByClassName('Cities')[0];
     if (input.value.length > 3) {
         for (let i = 0; i < rows.length; i++) {
-            let isContain = rows.item(i).childNodes[1].textContent.includes(input.value);
+            let isContain = rows.item(i).childNodes[1].textContent.toLowerCase().includes(input.value.toLowerCase());
             if (!isContain) rows.item(i).style.display = 'none';
-            if(table) table.style.display = 'none'
-            if(cities) cities.remove();
+            if (table) table.style.display = 'none'
+            if (cities) cities.remove();
         }
     } else {
         for (let i = 0; i < rows.length; i++) {
             rows.item(i).style.display = 'flex';
-            if(table) table.style.display = 'flex';
+            if (table) table.style.display = 'flex';
         }
     }
 }
 
+const select = document.getElementsByClassName('Selector')[0];
+select.addEventListener('change', async (e) => {
+    let time;
+    switch (e.target.value) {
+        case "regex":
+            time = Date.now()
+            data = await loadRegex('big.csv');
+            document.getElementsByClassName('Primary')[0].remove();
+            displayTable(filterList(data), "Container");
+            console.log(Date.now() - time);
+            break;
+        case "split":
+            time = Date.now()
+            data = await loadSplit('big.csv');
+            document.getElementsByClassName('Primary')[0].remove();
+            displayTable(filterList(data), "Container");
+            console.log(Date.now() - time);
+            break;
+
+        default:
+            console.log('Nothing loaded :(');
+            break;
+    }
+});
+
 document.addEventListener("DOMContentLoaded", async () => {
     data = await loadRegex('big.csv');
     displayTable(filterList(data), "Container");
+    citiesSorted = data.filter(item =>
+        item.level == '2'
+        && item.code.split('-')[3] != '000')
+        .sort((a, b) => a.name.localeCompare(b.name)
+        );
 })
 
 
 const loadRegex = async (name) => {
-    
+
     let result = await fetch('/data/' + name, {
         headers: {
             'Content-Type': 'text/plain',
@@ -75,13 +106,10 @@ const loadSplit = async (name) => {
     let list = [];
     for (let item of textNewLine) {
         let splitedItem = item.replace(/\"/g, '').split(';');
-
-        if (splitedItem[3] == '000' && splitedItem[2] == '000') {
-            list.push(new Item(
-                '' + splitedItem[0] + '-' + splitedItem[1] + '-' + splitedItem[2] + '-' + splitedItem[3],
-                splitedItem[6],
-                splitedItem[5]));
-        }
+        list.push(new Item(
+            '' + splitedItem[0] + '-' + splitedItem[1] + '-' + splitedItem[2] + '-' + splitedItem[3],
+            splitedItem[6],
+            splitedItem[5]));
     }
     return list;
 }
@@ -99,30 +127,9 @@ const displayList = (list) => {
 
 const displayTable = (list, name, tableName = null) => {
     let div = document.getElementsByClassName(name)[0];
-    let table = document.createElement('table');
+    //if(!table.classList.contains('Secondary') && tableName == null) div.firstChild.remove();
+    let table = generateTable(list);
     if (tableName != null) table.classList.add(tableName);
-
-    let thead = document.createElement('thead');
-    thead.classList.add('head');
-    let thr = thead.insertRow();
-    let thdCode = thr.insertCell();
-    thdCode.textContent = 'Код';
-    let thdName = thr.insertCell();
-    thdName.textContent = 'Название';
-    let thdLevel = thr.insertCell();
-    thdLevel.textContent = 'Уровень';
-    table.appendChild(thead);
-
-    for (let i = 0; i < list.length; i++) {
-        let tr = table.insertRow();
-        tr.classList.add('row');
-        for (const key in list[i]) {
-            let td = tr.insertCell();
-            td.textContent = list[i][key];
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    }
 
     if (!table.classList.contains('Secondary') && tableName == null) {
         table.onclick = (e) => {
@@ -138,28 +145,21 @@ const displayTable = (list, name, tableName = null) => {
             displaySubTable(data, activeTr);
         }
     }
-    div.appendChild(table);
+    if(tableName == null) div.prepend(table);
+    else div.appendChild(table);
+    
 }
 
 const displaySubTable = (list, elem) => {
 
     let existingTable = document.getElementsByClassName('Secondary')[0];
     if (existingTable) existingTable.remove();
-    let table = document.createElement('table');
-    table.classList.add('Secondary');
 
     let arr = filterList(list, elem, true);
     if (arr.length == 0) return;
     // Рисование таблицы
-    for (let i = 0; i < arr.length; i++) {
-        let tr = table.insertRow();
-        for (const key in arr[i]) {
-            let td = tr.insertCell();
-            td.textContent = arr[i][key];
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    }
+    let table = generateTable(arr, true);
+
     table.onclick = (e) => {
         let tr = e.target.closest('tr');
         if (tr.parentElement.classList.contains('Cities')) return;
@@ -218,16 +218,15 @@ const filterList = (list, elem = null, isRegion = false, isCity = false) => {
     let arr;
 
     if (isCity && elem != null) {
-        let [code1T, code2T, code3T] = elem.firstChild.textContent.split('-');
-
         arr = list.filter(item => {
-            let [code1, code2, code3, code4] = item.code.split('-');
-            return item.level == '2' && code1 == code1T && code2 == code2T  && code4 != '000';
+            let [code1, code2, _, code4] = item.code.split('-');
+            let [code1T, code2T] = elem.firstChild.textContent.split('-');
+            return item.level == '2' && code1 == code1T && code2 == code2T && code4 != '000';
         })
 
     } else if (isRegion && elem != null) {
-        let code1T = elem.firstChild.textContent.split('-')[0];
         arr = list.filter(item => {
+            let [code1T] = elem.firstChild.textContent.split('-');
             let [code1, code2, code3] = item.code.split('-');
             return item.level != '2' && code1 == code1T && code2 != '000' && code3 == '000';
         })
@@ -237,10 +236,31 @@ const filterList = (list, elem = null, isRegion = false, isCity = false) => {
     return arr;
 }
 
-
-
-
-const setLoading = () => {
-    if (document.body.style.filter) document.body.style.filter = '';
-    else document.body.style.filter = 'blur(2px)';
+const generateTable = (list, isSub = false) => {
+    let table = document.createElement('table');
+    if (!isSub) {
+        table.classList.add('Primary');
+        let thead = document.createElement('thead');
+        thead.classList.add('head');
+        let thr = thead.insertRow();
+        let thdCode = thr.insertCell();
+        thdCode.textContent = 'Код';
+        let thdName = thr.insertCell();
+        thdName.textContent = 'Название';
+        let thdLevel = thr.insertCell();
+        thdLevel.textContent = 'Уровень';
+        table.appendChild(thead);
+    }
+    else table.classList.add('Secondary');
+    for (let i = 0; i < list.length; i++) {
+        let tr = table.insertRow();
+        if (!isSub) tr.classList.add('row');
+        for (const key in list[i]) {
+            let td = tr.insertCell();
+            td.textContent = list[i][key];
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    return table;
 }
